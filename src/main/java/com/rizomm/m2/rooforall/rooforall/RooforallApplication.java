567b769d6e314a819fe143b92017a8ac48c76806
Ahder <benzinered@yahoo.fr>
2020-02-11 14:05:32 +0100

    intial commit with application.yml updated for local base

---
+++ src/main/java/com/rizomm/m2/rooforall/rooforall/RooforallApplication.java
@@ -0,0 +1,13 @@
+package com.rizomm.m2.rooforall.rooforall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RooforallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RooforallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rizomm/m2/rooforall/rooforall/RooforallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizomm.m2.rooforall.rooforall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RooforallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
